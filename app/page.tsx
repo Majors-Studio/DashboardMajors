6a19655bfa3183b3223082cc2599f341f177c23a
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-03-02 15:34:43 -0300

    Adiciona componente Header e atualiza importações

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import { Aside } from "@/src/components/Aside";
 import { BackgroundImage } from "@/src/components/BackgroundImage";
-import Status from "@/src/sections/Status/Status";
+import { Header, Status } from "@/src/sections";
 
 export default function Home() {
   return (
-    <main className="w-screen h-screen
-    flex flex-row justify-start items-start p-[20px] gap-[20px] overflow-hidden relative bg-[#060B26] z-[-10]
-    ">
+    <main className="w-screen h-screen flex flex-row justify-start items-start p-[20px] gap-[20px] overflow-hidden relative bg-[#060B26] z-[-10]">
       <BackgroundImage />
       <Aside />
 
-      <Status/>
+      <div>
+        <Header />
+        <Status />
+      </div>
     </main>
   );
 }
