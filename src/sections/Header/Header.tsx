c08d52659615612d4335baaa52851c34ce448eb6
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-03-02 15:51:18 -0300

    Adiciona componente NavigationInline e atualiza Header

--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import { NavigationInline } from "@/src/components/NavigationInline";
+import React from "react";
 
 const Header: React.FC = () => {
-  return <div />;
-}
+  const trilha = [
+    { title: 'Home', url: '/' },
+    { title: 'Dashboard' },
+  ];
+
+  return (
+    <div>
+      <section>
+        <NavigationInline trilha={trilha} />
+        <p>Dashboard</p>
+      </section>
+      <section></section>
+    </div>
+  );
+};
 
 export default Header;
