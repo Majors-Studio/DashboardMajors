8db2e73ea79b151e6a6cdc2078ffa839abeeab8b
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-03-02 17:42:08 -0300

    Atualizações na barra lateral e mensagem de ajuda

--- src/components/AsideMessage/AsideMessage.tsx
+++ src/components/AsideMessage/AsideMessage.tsx
@@ -9,22 +9,46 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ShieldQuestionIcon } from "lucide-react";
+import { HelpCircleIcon } from "lucide-react";
 
 const AsideMessage: React.FC = () => {
+  const bgPath = "/question/card_background.png";
+
   return (
-    <Card className="w-[350px]">
+    <Card
+      style={{
+        backgroundImage: `url(${bgPath})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+              width: "100%",
+     
+      }}
+    >
       <CardHeader>
-        <div>
-          <ShieldQuestionIcon size='20px' />
+        <div className=" w-[35px] h-[35px] rounded-[12px] bg-[#F5F5F5] flex items-center justify-center">
+          <div className="w-[24px] h-[24px] flex items-center justify-center rounded-[24px]">
+            <HelpCircleIcon
+              fill={"#0075FF"}
+              stroke={"white"}
+              strokeWidth={2}
+              className="w-7 h-7"
+            />
+          </div>
         </div>
       </CardHeader>
       <CardContent>
-        <CardTitle>Create adaproject</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle className="font-bold text-[14px]">
+          Precisa de Ajuda?
+        </CardTitle>
+        <CardDescription className="text-[12px]">
+          Por-favor confira a documentação para mais informações.
+        </CardDescription>
       </CardContent>
       <CardFooter>
-        <Button>Deploy</Button>
+        <Button className="w-full text-white bg-gradient-to-r from-[rgba(6, 11, 40, 0.74)] to-[rgba(10, 14, 35, 0.71)] rounded-[12px] bg-transparent">
+          DOCUMENTAÇÃO
+        </Button>
       </CardFooter>
     </Card>
   );
