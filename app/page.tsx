cf72e10998fb93eb0855f87fad29f16ac5735892
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2024-03-02 18:17:19 -0300

    Removido espaçamento desnecessário na página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,11 @@ import { Header, Status } from "@/src/sections";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen flex flex-row p-[20px] gap-[20px] overflow-hidden relative">
+    <main className="w-full h-screen flex flex-row p-[20px] overflow-hidden relative">
       <BackgroundImage />
       <Aside title="Majors Solutions" />
       <div className="
         flex flex-col
-        gap-[20px]
         w-full
       ">
         <Header />
